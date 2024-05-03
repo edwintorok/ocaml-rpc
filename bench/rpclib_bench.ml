cd15acaa06640d78dc32baf41e9d4751d0c29f34
Edwin Török <edwin.torok@cloud.com>
2024-05-03 14:50:34 +0100

    benchmark

---
+++ bench/rpclib_bench.ml
@@ -0,0 +1,50 @@
+let field = Printf.sprintf "field_%s_%d"
+let field_string i = field "str" i, Rpc.String (Printf.sprintf "short %d" i)
+let field_int i = field "i" i, Rpc.Int (Int64.of_int i)
+let field_bool i = field "b" i, Rpc.Bool (i mod 2 = 0)
+let field_dict i = field "d" i, Rpc.Dict (List.init 5 field_string)
+let large_dict_char i = Char.code 'A' + (i mod 32) |> Char.chr
+
+let field_large_dict i =
+  field "ld" i, Rpc.Dict [ "large", Rpc.String (String.init 32768 large_dict_char) ]
+
+
+let field_enum i =
+  field "e" i, Rpc.Enum (List.init 5 @@ fun i -> Rpc.String (Printf.sprintf "enum%d" i))
+
+
+let field_float i = field "f" i, Rpc.Float Float.pi
+
+let rpc =
+  (* approximately matches the mix of field types in XAPI VM *)
+  Rpc.Dict
+    (List.flatten
+       [ List.init 29 field_string
+       ; List.init 1 field_float
+       ; List.init 9 field_bool
+       ; List.init 10 field_int
+       ; List.init 6 field_dict
+       ; List.init 1 field_large_dict
+       ; List.init 15 field_enum
+       ])
+
+
+let bench_protocol name of_string to_string =
+  let serialized = to_string rpc in
+  let open Bechamel in
+  let encode () = to_string rpc
+  and decode () = of_string serialized in
+  Test.make_grouped ~name
+  [ Test.make ~name:("to_string") Staged.(stage encode)
+  ; Test.make ~name:("of_string") Staged.(stage decode)
+  ]
+
+let tests =
+  Bechamel.Test.make_grouped ~name:"rpclib"
+  [ bench_protocol "xmlrpc" Xmlrpc.of_string Xmlrpc.to_string
+  ; bench_protocol "jsonrpc" Jsonrpc.of_string Jsonrpc.to_string
+  ; bench_protocol "csexp" Csexprpc.of_string Csexprpc.to_string
+  ]
+
+let () =
+  Bechamel_simple_cli.cli tests
